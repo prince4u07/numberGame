4eeca6da19fc99732bdd281e94e3df2389e7d3f8
Prince Kumar Vishwakarma <princeisonline4u@gmail.com>
2025-07-25 20:47:58 +0530

    Add files via upload

---
+++ numbergame.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class numbergame{
+	public static void main(String[]args){
+		Random random=new Random();
+		Scanner s1= new Scanner(System.in);
+		int guess;
+		int attempts=0;
+		int randomNumber=random.nextInt(1,11);
+		System.out.println("guess no2 between 1 to 10: ");
+		do{
+			System.out.print("enter a guess: ");
+			guess=s1.nextInt();
+			attempts++;
+			
+			if(guess<randomNumber){
+				System.out.print("TOO LOW! TRY AGAIN");
+			}
+			else if(guess>randomNumber){
+				System.out.println("CORRECT!THE NUMBER WAS"+randomNumber);
+			}else{
+				System.out.println("correct !number was "+randomNumber);
+				System.out.println("# of attempts: " + attempts);
+			}
+				
+		}while(guess != randomNumber);
+	}
+}
